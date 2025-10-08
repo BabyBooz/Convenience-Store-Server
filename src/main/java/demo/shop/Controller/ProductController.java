3c781a0440089d6918658b6b72b3e52a744243be
Linh Nghiêm <linhnghiem202@gmail.com>
2025-10-08 19:07:58 +0700

    add code

--- src/main/java/demo/shop/Controller/ProductController.java
+++ src/main/java/demo/shop/Controller/ProductController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,7 +57,7 @@ public class ProductController {
   }
 
   @PostMapping("/create")
-  public ApiResponse<ProductResponse> createProduct(ProductRequest request){
+  public ApiResponse<ProductResponse> createProduct(@RequestBody  ProductRequest request){
     ProductResponse response = productService.createProduct(request);
     return ApiResponse.<ProductResponse>builder()
         .code(1000)
